029e27a0186106d9eeb1bc494a0ac3e732adfc60
Mike Hewitt <matroxmike@icloud.com>
2020-05-22 03:44:44 +0000

    new version of main ...

--- main.cs
+++ main.cs
@@ -2,6 +2,10 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
+<<<<<<< HEAD
     Console.WriteLine ("Hello World 2 corona ");
+=======
+    Console.WriteLine ("Hello World 2 corona");
+>>>>>>> origin/master
   }
 }
